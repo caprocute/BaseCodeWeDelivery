b77486e79d1f550f0fdccb2607af2ffec7cecd22
Hoàng Khắc Hiếu <hoangkhachieu96@gmail.com>
2018-04-07 21:21:42 +0700

    reworkk code
    adfa

--- app/src/main/java/com/example/hoang/myapplication/LauncherActivity.java
+++ app/src/main/java/com/example/hoang/myapplication/LauncherActivity.java
@@ -1,13 +1,13 @@
-package com.example.hoang.wedelivery;
+package com.example.hoang.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LauncherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_launcher);
     }
 }
